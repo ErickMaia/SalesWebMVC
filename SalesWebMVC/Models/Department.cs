6d69acd95e0f608e1a01bcd81d01148ac4da4727
ErickMaia <erickmaiatmariano@gmail.com>
2020-12-01 06:30:54 -0300

    Outras entidades e segunda migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -9,5 +9,27 @@ namespace SalesWebMVC.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int iD, string name)
+        {
+            ID = iD;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initialDate, DateTime finalDate)
+        {
+            return (from s in Sellers select s.TotalSales(initialDate, finalDate)).DefaultIfEmpty().Sum();
+        }
+
     }
 }
---
+++ SalesWebMVC/Models/SalesRecord.cs
@@ -0,0 +1,17 @@
+﻿using SalesWebMVC.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class SalesRecord
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+    }
+}
